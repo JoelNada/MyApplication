977158551567ad6d40f8918ceea57af2f88c29bd
JoelNada20 <66004734+JoelNada@users.noreply.github.com>
2024-04-29 10:20:44 +0530

    war file and SBSI dependency

--- src/main/java/com/myapp/MyApplication/MyApplication.java
+++ src/main/java/com/myapp/MyApplication/MyApplication.java
@@ -2,9 +2,15 @@ package com.myapp.MyApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
